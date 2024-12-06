e281af3814bab917e296604479ac14c57e211779
Meleoidas <infiniteriper123@gmail.com>
2024-12-06 22:54:11 +0200

    part 2

--- AoC Day 2.cpp
+++ AoC Day 2.cpp
@@ -1,3 +1,5 @@
+// worst code humanity has ever seen, but it works 👍
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -6,35 +8,92 @@
 
 using namespace std;
 
-bool isRowSafe(vector<int>& rowStringVector) {
+
+bool check(vector<int>& checkVector){
     int increasing = 0, decreasing = 0;
 
-    for (int i = 0; i < rowStringVector.size() - 1; i++){
-       bool isIncreasing = (rowStringVector[i+1] > rowStringVector[i]);
-       if(isIncreasing){
+    for(int i = 0; i < checkVector.size() - 1; i++){
+       bool isIncreasing = (checkVector[i+1] > checkVector[i]);
+       if(checkVector[i+1] > checkVector[i]){
         increasing++;
-       }else{
+       }else if(checkVector[i+1] < checkVector[i]){
         decreasing++;
+       }else{
+        return false;
        }
     }
     if(increasing == decreasing){return false;}
-    bool isIncreasing = (increasing > decreasing);
+    if(increasing>0 && decreasing>0){return false;}
+    
+    for(int i = 0; i < checkVector.size() - 1; i++){
+        int diff = checkVector[i+1] - checkVector[i];
 
-    for(int i=0; i < rowStringVector.size() - 1; i++){
-        int diff = rowStringVector[i+1] - rowStringVector[i];
-
-        if(diff < 0 && isIncreasing){
-            return false;
-        }else if(diff > 0 && !isIncreasing){
-            return false;
-        }
-
-        if(1 > abs(diff) || abs(diff) > 3){
-            return false;
+        if(1 > abs(diff) || abs(diff) > 3) {
+          return false;
         }
     }
     
     return (true);
+}
+
+bool isRowSafe(vector<int>& rowStringVector) {
+    int increasing = 0, decreasing = 0;
+
+    for(int i = 0; i < rowStringVector.size() - 1; i++){
+       bool isIncreasing = (rowStringVector[i+1] > rowStringVector[i]);
+       if(rowStringVector[i+1] > rowStringVector[i]){
+        increasing++;
+       } else if(rowStringVector[i+1] < rowStringVector[i]) {
+        decreasing++;
+       } else {
+        vector<int> check_incdec = rowStringVector;
+        check_incdec.erase(check_incdec.begin() + i);
+        if(!check(check_incdec)) {return false;}
+       }
+    }
+
+    if(increasing == decreasing){return false;}
+    if(increasing > 0 && decreasing > 0){
+      bool isIncreasing = (increasing > decreasing);
+       if(isIncreasing){
+        for(int i = 0; i < rowStringVector.size() - 1; i++) {
+        if(rowStringVector[i+1] < rowStringVector[i]){
+          vector<int> check_first = rowStringVector;
+          check_first.erase(check_first.begin() + i);
+          vector<int> check_second = rowStringVector;
+          check_second.erase(check_second.begin() + i+1);
+
+          if(!check(check_first) && !check(check_second)) {return false;}
+        }
+       }
+       }else{
+        for(int i = 0; i < rowStringVector.size() - 1; i++) {
+        if(rowStringVector[i+1] > rowStringVector[i]){
+          vector<int> check_first = rowStringVector;
+          check_first.erase(check_first.begin() + i);
+          vector<int> check_second = rowStringVector;
+          check_second.erase(check_second.begin() + i+1);
+
+          if(!check(check_first) && !check(check_second)) {return false;}
+        }
+       }
+       }
+      }
+
+    for(int i = 0; i < rowStringVector.size() - 1; i++) {
+        int diff = rowStringVector[i+1] - rowStringVector[i];
+
+        if(1 > abs(diff) || abs(diff) > 3) {
+            vector<int> check_first = rowStringVector;
+            check_first.erase(check_first.begin() + i);
+            vector<int> check_second = rowStringVector;
+            check_second.erase(check_second.begin() + i + 1);
+
+            if(!check(check_first) && !check(check_second)) {return false;}
+        }
+    }
+    
+    return true;
 }
 
 int main(){
