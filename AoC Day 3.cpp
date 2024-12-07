1bc33cde8c569680f0185552c8811eeff557960e
Meleoidas <infiniteriper123@gmail.com>
2024-12-07 20:02:01 +0200

    d3 p2

--- AoC Day 3.cpp
+++ AoC Day 3.cpp
@@ -13,10 +13,34 @@ int main(){
   }
 
   regex rgx("mul\\((\\d+),(\\d+)\\)");
-  smatch match;
-  while(regex_search(str, match, rgx)){
-    sum += stoi(match[1].str()) * stoi(match[2].str());
-    str = match.suffix().str();
+  regex rgxDo("do\\(\\)");
+  regex rgxDont("don't\\(\\)");
+
+  smatch match, matchDo, matchDont;
+
+  bool Do = true;
+
+  while(true){
+    string str1 = "", str2 = "";
+    if(Do && regex_search(str, matchDont, rgxDont)){
+      str1 = matchDont.suffix().str();
+    }
+    if(Do && regex_search(str, match, rgx)){
+      str2 = match.suffix().str();
+    }else if(!Do && regex_search(str, matchDo, rgxDo)){
+      str = matchDo.suffix().str();
+      Do = true;
+    }
+    if(Do && str1.size() > str2.size()){
+      str = str1;
+      Do = false;
+    }else if(Do && str1.size() < str2.size()){
+      sum += stoi(match[1].str()) * stoi(match[2].str());
+      str = str2;
+    }
+    if(!regex_search(str, matchDont, rgxDont) && !regex_search(str, match, rgx) && !regex_search(str, matchDo, rgxDo)){
+      break;
+    }
   }
 
   cout<<sum;
